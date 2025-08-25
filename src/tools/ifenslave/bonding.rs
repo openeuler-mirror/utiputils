bc1ebb1c893da090e7ee401b1708b6d6b9e7b6f5
ut-wanglujun <wanglujun@uniontech.com>
2025-08-25 17:14:52 +0800

    实现从主接口中释放指定的从接口，解除绑定关系

--- src/tools/ifenslave/bonding.rs
+++ src/tools/ifenslave/bonding.rs
@@ -70,4 +70,72 @@ impl NetworkInterface {
 
         Ok(())
     }
+
+    // Release the slave interface from the master interface
+    pub async fn release(&self, master: &str, slave: &str) -> Result<(), io::Error> {
+        // Check the status of the slave interface
+        let slave_flags = match self.get_interface_flags(slave).await {
+            Ok(flags) => flags,
+            Err(_) => {
+                return Err(io::Error::new(
+                    io::ErrorKind::NotFound,
+                    format!("Slave interface '{}' not found", slave),
+                ))
+            }
+        };
+
+        // Check if the slave interface is a slave device
+        if slave_flags & libc::IFF_SLAVE as u32 == 0 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!(
+                    "Invalid operation: Specified slave interface '{}' is not a slave device",
+                    slave
+                ),
+            ));
+        }
+
+        // Release the slave interface via sysfs
+        let path = format!("/sys/class/net/{}/bonding/slaves", master);
+        let contents = format!("-{}", slave);
+        debug!("path: {}, contents: {}", path, contents);
+        let result = fs::write(&path, contents);
+
+        if result.is_err() {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!(
+                    "Master interface '{}': Failed to release slave interface '{}': {}",
+                    master,
+                    slave,
+                    result.unwrap_err()
+                ),
+            ));
+        }
+
+        // Older ABI requires disabling the slave interface
+        if self.abi_ver() < 1 && (self.set_interface_down(slave).await).is_err() {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("Slave interface '{}': Failed to disable interface", slave),
+            ));
+        }
+
+        // Set to default MTU
+        if (self.set_interface_mtu(slave, 1500).await).is_err() {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("Slave interface '{}': Failed to set default MTU", slave),
+            ));
+        }
+
+        if self.verbose() {
+            println!(
+                "Master interface '{}': Successfully released slave interface '{}'",
+                master, slave
+            );
+        }
+
+        Ok(())
+    }
 }
